41a5ffe12e032932683f3a95ddd06b1ea6eb0399
aa-ag <aaron.aguerrevere@gmail.com>
2021-09-05 14:06:41 -0500

    added url to landing/home page on linkedin -- depends on user being logged in

--- script.py
+++ script.py
@@ -3,11 +3,14 @@ import selenium
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+url = 'https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit'
 
 
 ############------------ FUNCTION(S) ------------############
+def delete_inmails():
+    pass
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    delete_inmails()
