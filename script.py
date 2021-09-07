365aedfd1d9f7e7176497a45127af73fba0020ae
aa-ag <aaron.aguerrevere@gmail.com>
2021-09-06 20:29:30 -0500

    added time import and sleep seconds in between steps to avoid bot detection

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 ############------------ IMPORTS ------------############
+import time
 from selenium import webdriver
 from settings import username, password
 
@@ -21,11 +22,20 @@ def delete_inmails():
     # opens url
     driver.get(url)
 
+    # wait to avoid bot-detection
+    time.sleep(4)
+
     # input username into username field
     driver.find_element_by_id("username").send_keys(username)
 
+    # wait to avoid bot-detection
+    time.sleep(3)
+
      # input password into password field
     driver.find_element_by_id("password").send_keys(password)
+
+    # wait to avoid bot-detection
+    time.sleep(2)
 
     # click on "Sign in" button
     driver.find_element_by_class_name("btn__primary--large from__button--floating")
